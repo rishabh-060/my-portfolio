640a054c4d2ceb8883e8e405b001591a27a0058c
Rishabh Verma <verma.rishabh924@gmail.com>
2025-01-10 19:51:13 +0530

    Add Certification Section & Changing Fonts & Profiles

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let img = document.getElementById('hero-img');
 let cv = document.getElementById('cv');
 
 
-
 gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
@@ -43,14 +42,6 @@ img.addEventListener("mouseleave", function(){
     img.style.opacity = "1";
 })
 
-// cv.addEventListener("mouseenter", function(){
-//     cv.innerHTML = `<i class="ri-download-2-fill"></i>`
-// })
-
-// cv.addEventListener("mouseleave", function(){
-//     cv.innerHTML = `<span><video src="icons/cv.mp4" muted autoplay loop></video></span>`
-    
-// });
 
 function loadingAnimation(){
     gsap.from("#hero img,#hero h2,#hero p,#hero #mail", {
@@ -79,95 +70,78 @@ function loadingAnimation(){
         delay : 0.3,
     })
 
-    gsap.from("#pr-dets h3, #pr-dets h5",{
-        y : 50,
-        opacity : 0,
-        delay : 0.3,
-        duration : 0.3,
-        stagger : 0.1,
-        // repeat : -1,
-        // yoyo: true,
-        // ease: "sine.inOut",
-        scrollTrigger : {
-            trigger: ".project-item",
-            start: 'top bottom', // When the top of the section hits the bottom of the viewport
-            end: 'bottom top', // When the bottom of the section hits the top of the viewport
-            scrub: true, // Smooth animation linked to scroll progress
-            // markers: true,
-            scroller : "#right-aside",
-            markers: false
-        }
+    // gsap.from("#pr-dets h3, #pr-dets h5",{
+    //     y : 50,
+    //     opacity : 0,
+    //     delay : 0.3,
+    //     duration : 0.3,
+    //     stagger : 0.1,
+    //     // repeat : -1,
+    //     // yoyo: true,
+    //     // ease: "sine.inOut",
+    //     scrollTrigger : {
+    //         trigger: ".project-item",
+    //         start: 'top bottom', // When the top of the section hits the bottom of the viewport
+    //         end: 'bottom top', // When the bottom of the section hits the top of the viewport
+    //         scrub: true, // Smooth animation linked to scroll progress
+    //         // markers: true,
+    //         scroller : "#right-aside",
+    //         markers: false
+    //     }
         
-    })
+    // })
     
-    gsap.from("#pr-img img",{
-        y : 50,
-        opacity : 0,
-        scale : 0,
-        delay : 0.1,
-        duration : 0.4,
-        stagger : 0.1,
-        scrollTrigger : {
-            trigger: ".project-item",
-            start: 'top bottom', // When the top of the section hits the bottom of the viewport
-            end: 'bottom top', // When the bottom of the section hits the top of the viewport
-            scrub: true, // Smooth animation linked to scroll progress
-            // markers: true,
-            scroller : "#right-aside",
-            markers: false
-        }
-    })
+    // gsap.from("#pr-img img",{
+    //     y : 50,
+    //     opacity : 0,
+    //     scale : 0,
+    //     delay : 0.1,
+    //     duration : 0.4,
+    //     stagger : 0.1,
+    //     scrollTrigger : {
+    //         trigger: ".project-item",
+    //         start: 'top bottom', // When the top of the section hits the bottom of the viewport
+    //         end: 'bottom top', // When the bottom of the section hits the top of the viewport
+    //         scrub: true, // Smooth animation linked to scroll progress
+    //         // markers: true,
+    //         scroller : "#right-aside",
+    //         markers: false
+    //     }
+    // })
 
-    gsap.from(".education-item h3,.education-item p",{
-        y : 80,
-        opacity : 0,
-        delay : 0.1,
-        duration : 0.5,
-        stagger : 0.1,
-        scrollTrigger : {
-            trigger: ".education-item",
-            start: 'top bottom', // When the top of the section hits the bottom of the viewport
-            end: 'bottom top', // When the bottom of the section hits the top of the viewport
-            scrub: true, // Smooth animation linked to scroll progress
-            // markers: true,
-            scroller : "#right-aside",
-            markers: false
-        }
-    })
+    // gsap.from(".education-item h3,.education-item p",{
+    //     y : 80,
+    //     opacity : 0,
+    //     delay : 0.1,
+    //     duration : 0.5,
+    //     stagger : 0.1,
+    //     scrollTrigger : {
+    //         trigger: ".education-item",
+    //         start: 'top bottom', // When the top of the section hits the bottom of the viewport
+    //         end: 'bottom top', // When the bottom of the section hits the top of the viewport
+    //         scrub: true, // Smooth animation linked to scroll progress
+    //         // markers: true,
+    //         scroller : "#right-aside",
+    //         markers: false
+    //     }
+    // })
 
-    gsap.from(".experience-item h3,.experience-item p",{
-        y : 50,
-        opacity : 0,
-        delay : 0.3,
-        duration : 0.6,
-        stagger : 0.1,
-        scrollTrigger : {
-            trigger: ".experience-item",
-            start: 'top 70%', // When the top of the section hits the bottom of the viewport
-            end: 'bottom 70%', // When the bottom of the section hits the top of the viewport
-            scrub: true, // Smooth animation linked to scroll progress
-            // markers: true,
-            scroller : "#right-aside",
-            markers: false
-        }
-    })
-    
-    gsap.from("#contact a",{
-        x : 50,
-        opacity : 0,
-        delay : 0.3,
-        duration : 1.2,
-        stagger : 0.1,
-        scrollTrigger : {
-            trigger: "#contact a",
-            start: 'top 90%', // When the top of the section hits the bottom of the viewport
-            end: 'bottom 90%', // When the bottom of the section hits the top of the viewport
-            scrub: true, // Smooth animation linked to scroll progress
-            // markers: true,
-            scroller : "#right-aside",
-            markers: false
-        }
-    })
+    // gsap.from(".experience-item h3,.experience-item p",{
+    //     y : 50,
+    //     opacity : 0,
+    //     delay : 0.3,
+    //     duration : 0.6,
+    //     stagger : 0.1,
+    //     scrollTrigger : {
+    //         trigger: ".experience-item",
+    //         start: 'top 70%', // When the top of the section hits the bottom of the viewport
+    //         end: 'bottom 70%', // When the bottom of the section hits the top of the viewport
+    //         scrub: true, // Smooth animation linked to scroll progress
+    //         // markers: true,
+    //         scroller : "#right-aside",
+    //         markers: false
+    //     }
+    // })
 
 }
 
